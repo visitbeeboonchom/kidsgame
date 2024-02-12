ea8bed48be39c473ece36f3cd329920a956c2cca
Visitbeeboonchom <visit@tsu.ac.th>
2024-02-12 23:38:11 +0700

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,119 @@
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final Map<String, bool> score = {};
+  final Map<String, Color> choices = {
+    '🍎': Colors.red,
+    '🥒': Colors.green,
+    '🔵': Colors.blue,
+    '🍍': Colors.yellow,
+    '🍊': Colors.orange,
+    '🍇': Colors.purple,
+    '🥥': Colors.brown,
+  };
+  int index = 0;
+  final play = AudioPlayer();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: choices.keys.map((element) {
+                return Expanded(
+                  child: Draggable<String>(
+                    data: element,
+                    child: Movable(element),
+                    feedback: Movable(element),
+                    childWhenDragging: Movable('🐰'),
+                  ),
+                );
+              }).toList(),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: choices.keys.map((element) {
+                return buildTarget(element);
+              }).toList()
+                ..shuffle(Random(index)),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.refresh),
+            onPressed: () {
+              setState(() {
+                score.clear();
+                index++;
+              });
+            }));
+  }
+
+  Widget buildTarget(emoji) {
+    return DragTarget<String>(
+      builder: (context, incoming, rejects) {
+        if (score[emoji] == true) {
+          return Container(
+            color: Colors.white,
+            child: Text('Congratulations'),
+            alignment: Alignment.center,
+            height: 80,
+            width: 200,
+          );
+        } else {
+          return Container(
+            color: choices[emoji],
+            height: 80,
+            width: 200,
+          );
+        }
+      },
+      onWillAccept: (data) => data == emoji,
+      onAccept: (data) {
+        setState(() {
+          score[emoji] = true;
+          play.play(AssetSource('clap.mp3'));
+        });
+      },
+      onLeave: (data) {},
+    );
+  }
+}
+
+class Movable extends StatelessWidget {
+  final String emoji;
+  Movable(this.emoji);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        alignment: Alignment.center,
+        height: 50,
+        padding: EdgeInsets.all(15),
+        child: Text(
+          emoji,
+          style: TextStyle(color: Colors.black, fontSize: 60),
+        ),
+      ),
+    );
+  }
+}
